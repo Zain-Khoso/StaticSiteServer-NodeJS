d579af1faf714f0068375f8fe667e42130af0f53
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-02-13 14:32:59 +0500

    Basic app router

--- src/server.js
+++ src/server.js
@@ -5,14 +5,28 @@ const fs = require("fs");
 const path = require("path");
 
 const server = function (request, response) {
-    response.statusCode = 200;
+    // Setting the content type of our response.
     response.setHeader("Content-Type", "text/html");
 
-    // Serving an HTML file.
-    const filePath = path.join("public", "index.html");
-    const data = fs.readFileSync(filePath, "utf8");
+    if (request.url === "/") {
+        // Setting a success status code for our response.
+        response.statusCode = 200;
 
-    response.end(data.toString());
+        // Serving an HTML file.
+        const filePath = path.join("public", "index.html");
+        const data = fs.readFileSync(filePath, "utf8");
+
+        response.end(data.toString());
+    } else {
+        // Setting a 404 status code for our response.
+        response.statusCode = 404;
+
+        // Serving an HTML file.
+        const filePath = path.join("public", "error.html");
+        const data = fs.readFileSync(filePath, "utf8");
+
+        response.end(data.toString());
+    }
 };
 
 module.exports = server;
