2f95c16164f050661a5975fb783f2eba9350c972
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-02-13 14:22:52 +0500

    Serving HTML files

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 // This file holds the server function that which is basically our whole server.
 
+// Imports
+const fs = require("fs");
+const path = require("path");
+
 const server = function (request, response) {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html");
-    response.end("<h1>Success</h1>");
+
+    // Serving an HTML file.
+    const filePath = path.join("public", "index.html");
+    const data = fs.readFileSync(filePath, "utf8");
+
+    response.end(data.toString());
 };
 
 module.exports = server;
